8f33f874f97a716f137c8a7fb40e1982080df648
Mike Land <mikepland@gmail.com>
2014-10-24 15:36:24 -0500

    Initial Commit

---
+++ multiply2MM/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  multiply2MM
+//
+//  Created by MIKE LAND on 10/24/14.
+//  Copyright (c) 2014 MIKE LAND. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
